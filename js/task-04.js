7628bd4b4773ea0d0f86ae5b6790a03f05389bff
Diana Bukovska <dianavladimirovna1409@gmail.com>
2022-09-27 22:49:06 +0300

    task-04

---
+++ js/task-04.js
@@ -0,0 +1,19 @@
+const buttonDecrement = document.querySelector('button[data-action="decrement"]');
+const buttonIncrement = document.querySelector('button[data-action="increment"]');
+const valueCounter = document.querySelector('#value');
+
+let value = valueCounter.textContent;
+value = 0;
+
+const valueDecrement = () => {
+  value -= 1;
+  return (valueCounter.textContent = value);
+};
+
+const valueIncrement = () => {
+  value += 1;
+  return (valueCounter.textContent = value);
+};
+
+buttonDecrement.addEventListener('click', valueDecrement);
+buttonIncrement.addEventListener('click', valueIncrement);
