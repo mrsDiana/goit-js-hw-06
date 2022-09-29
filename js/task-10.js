dcf941fbd6b09665264f7fe8ad3974f8fb06d4be
Diana Bukovska <dianavladimirovna1409@gmail.com>
2022-09-30 00:15:57 +0300

    corrected task-10

--- js/task-10.js
+++ js/task-10.js
@@ -2,19 +2,19 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const startWidthValue = 10;
-const startHeightValue = 10;
+const startWidthValue = 30;
+const startHeightValue = 30;
 const buttonCreate = document.querySelector('[data-create]');
 const buttonDestroy = document.querySelector('[data-destroy]');
 const listOfElement = document.querySelector('#boxes');
 const inputValue = document.querySelector('#controls').querySelector('input');
 
-const createBlock = () => {
+const createBoxes = () => {
   let arrayOfNumbers = [];
   for (let i = 1; i <= inputValue.value; i += 1) {
     const divElement = document.createElement('div');
-    const widthValue = startWidthValue * i;
-    const heightValue = startHeightValue * i;
+    const widthValue = startWidthValue + i*10;
+    const heightValue = startHeightValue + i*10;
     divElement.style.background = getRandomHexColor();
     divElement.style.width = `${widthValue}px`;
     divElement.style.height = `${heightValue}px`;
@@ -23,9 +23,9 @@ const createBlock = () => {
   listOfElement.prepend(...arrayOfNumbers);
 };
 
-const reset = () => {
+const destroyBoxes = () => {
   listOfElement.innerHTML = '';
 };
 
-buttonCreate.addEventListener('click', createBlock);
-buttonDestroy.addEventListener('click', reset);
+buttonCreate.addEventListener('click', createBoxes);
+buttonDestroy.addEventListener('click', destroyBoxes);
