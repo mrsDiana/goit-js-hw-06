08ce1eec80fc0de112ad6988f8483b0153a02f78
Diana Bukovska <dianavladimirovna1409@gmail.com>
2022-09-29 22:23:32 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorBody = document.querySelector('body');
+const changeColorButton = document.querySelector('.change-color');
+const textColor = document.querySelector('.color');
+
+const changeColorClick = () => {
+  colorBody.style.backgroundColor = getRandomHexColor();
+  textColor.textContent = getRandomHexColor();
+};
+
+changeColorButton.addEventListener('click', changeColorClick);
